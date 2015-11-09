1a9031227126c9407a4105927a1d6b760c51ac0e
alu0100499377 <alu0100499377@ull.edu.es>
2015-11-09 17:58:19 +0000

    Se pude insertar mas de un elemento

--- spec/prct07_spec.rb
+++ spec/prct07_spec.rb
@@ -5,7 +5,10 @@ module Prct07
  describe Prct07::Bibliografia do
   before :each do
     @l = Prct07::Bibliografia.new('Cervantes', 'El Quijorte', '12345', '1605')
+    @l2 = Prct07::Bibliografia.new('Tolkien', 'El Hobbit', '5678', '1945')
+    
     @node1 = Node.new(@l, nil)
+    @node2 = Node.new(@l2, nil)
   end
   
   it 'Debe existir un autor' do
@@ -22,6 +25,14 @@ module Prct07
   it 'Debe existir un Nodo con su valor y su siguiente' do
     expect(@lista.cabeza != nil && @lista.next == nil && @lista.anterior == nil)
   end
+  
+  it 'Se puede insertar mas de un elemento' do
+    @lista.push(@node1)
+    @lista.push(@node2)
+    expect(@lista.cabeza).to eq(@node2)
+  end
+    
+    
  end# end describe list
  
 end
